ea1541f13e74d96e061560e72e670d88754f856d
André Luiz Guimarães de Souza Leite <andre.guimaraes.leite@gmail.com>
2013-12-07 15:18:44 -0300

    Initial setup for the Alphabet class.

---
+++ src/core/languages/Alphabet.java
@@ -0,0 +1,49 @@
+package core.languages;
+
+import java.util.concurrent.ConcurrentSkipListSet;
+
+/**
+ * This class will represent an alphabet. 
+ * An alphabet can hold symbols of any time.
+ * @author andreluiz
+ *
+ * @param <T> Type of the symbols to be held.
+ */
+public class Alphabet<T> extends ConcurrentSkipListSet<T> {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -7654544380638271534L;
+
+	/**
+	 * Name of the alphabet.
+	 */
+	private String alias;
+	
+	/**
+	 * Default constructor. It sets alias to null.
+	 */
+	public Alphabet() {
+		super();
+		this.alias = null;
+	}
+	
+	/**
+	 * Constructor to create an alphabet with the given name.
+	 * @param alias Alphabet's name.
+	 */
+	public Alphabet(String alias) {
+		super();
+		this.alias = alias;
+	}
+
+	public String getAlias() {
+		return alias;
+	}
+	
+	public void setAlias(String alias) {
+		this.alias = alias;
+	}
+	
+}
