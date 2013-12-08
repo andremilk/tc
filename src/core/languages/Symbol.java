4723509ec19e32ec8819dde919e02a4df7627f4d
André Luiz Guimarães de Souza Leite <andre.guimaraes.leite@gmail.com>
2013-12-08 11:09:03 -0300

    Documents Symbol.

--- src/core/languages/Symbol.java
+++ src/core/languages/Symbol.java
@@ -1,15 +1,36 @@
 package core.languages;
 
+/**
+ * Class representing symbols. These symbols will populate alphabets.
+ * @author andreluiz
+ *
+ * @param <T> A Symbol can be any kind of object.
+ */
 public class Symbol<T> {
 
+	/**
+	 * This attribute will hold the object type of the symbol.
+	 */
 	private Class<? extends Object> type;
+	
+	/**
+	 * String value/alias to the symbol.
+	 */
 	private String value;
 	
+	/**
+	 * Default constructor for a Symbol.
+	 * @param obj Object to be used on the construction. It can be an object of any type.
+	 */
 	public Symbol(Object obj) {
 		this.value = obj.toString();
 		type = obj.getClass();
 	}
 
+	/**
+	 * 
+	 * @return type of the object used on the construction.
+	 */
 	public Class<? extends Object> getType() {
 		return type;
 	}
